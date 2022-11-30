22dab72f7309bdbfa79a14b310a0836c08f3edf0
mindaugas.arsulis <mindaugas.arsulis@insoft.lt>
2022-11-30 08:16:42 +0200

    Add h2 database, clean up pom dependencies

--- app/src/main/java/lt/reminder/app/SpringBootWebApplication.java
+++ app/src/main/java/lt/reminder/app/SpringBootWebApplication.java
@@ -2,10 +2,9 @@ package lt.reminder.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebApplication extends SpringBootServletInitializer {
+public class SpringBootWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
